1f63af180ade19d416b0ae573c219c92c2de6161
zerosand1 <priyansh.23raj@gmail.com>
2025-04-23 19:37:32 +0530

    screen display work

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
     
     
     
